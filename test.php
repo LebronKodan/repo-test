e30ec4a92e8eaf58a9601efeb484c480906bf58f
Lab/Study <xhy1994512@163.com>
2013-08-04 22:33:39 +0800

    testResult

---
+++ test.php
@@ -0,0 +1,27 @@
+<?php 
+	$num = $_POST['number'];
+?>
+<table id="table_<?php print $num;?>" class="table table-bordered table-hover">
+	<thead>
+		<tr>
+			<th><?php print $num;?></th>
+			<th><?php print $num;?></th>
+			<th>编号</th>
+			<th>编号</th>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<td>编号</td>
+			<td>编号</td>
+			<td>编号</td>
+			<td>编号</td>
+		</tr>
+		<tr>
+			<td>编号</td>
+			<td>编号</td>
+			<td>编号</td>
+			<td>编号</td>
+		</tr>
+	</tbody>
+</table>
