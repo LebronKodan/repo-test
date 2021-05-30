41f6154bbea0ae3652ab4aa6ab4b75a91cd30e9f
Amstrong <xhy1994512@163.com>
2021-05-30 22:05:01 +0800

    add new file, and remove stale file

--- main.java
+++
@@ -1,8 +0,0 @@
-public class Hello {
-	public static void main(String[] args) {
-		System.out.println("Hello world");
-		System.out.println("This is added in 2013.09.05");
-		System.out.println("A new line");
-		String a = "New local branch";
-	}
-}
